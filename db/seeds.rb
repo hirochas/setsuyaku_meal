2c6df61923761a77f1cd302dc2e872b16915c035
HiromiYoshizaki <115017300+hirochas@users.noreply.github.com>
2023-03-30 02:24:26 +0000

    [Add]Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,20 @@ Admin.create!(
     email: 'admin@admin.com',
     password: 'adminok',
 )
+
+customers = Customer.create!(
+  [
+    {email: 'satou@test.com', name: '佐藤 ', password: 'password'},
+    {email: 'yamamoto@test.com', name: '山本', password: 'password'},
+    {email: 'suzuki@test.com', name: '鈴木', password: 'password', }
+    ]
+)
+
+genres = Genre.create!(
+  [
+    {name: '卵を使った料理'},
+    {name: '麺を使った料理'},
+    {name: 'お肉を使った料理'},
+    {name: '野菜を使った料理'}
+  ]
+)
