3c0c2823bdebb52b2cb41ce0f7414338f6991e3e
HiromiYoshizaki <115017300+hirochas@users.noreply.github.com>
2023-03-30 00:55:29 +0000

    [Add]seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+｝# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
@@ -10,4 +10,21 @@
 Admin.create!(
     email: 'admin@admin.com',
     password: 'adminok',
+)
+
+customers = Customer.create!(
+  [
+    {email: 'satou@test.com', name: '佐藤 ', password: 'password'},
+    {email: 'yamamoto@test.com', name: '山本', password: 'password'},
+    {email: 'suzuki@test.com', name: '鈴木', password: 'password', }
+    ]
+)
+
+genres = Genre.create!(
+  [
+    {name: '卵を使った料理'},
+    {name: '麺を使った料理'},
+    {name: 'お肉を使った料理'},
+    {name: '野菜を使った料理'}
+  ]
 )
